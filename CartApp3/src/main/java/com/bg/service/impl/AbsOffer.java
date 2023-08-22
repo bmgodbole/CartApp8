de87d9349f6eae495a02c2938ad3f5f23ac19ad0
bmgodbole <75110014+bmgodbole@users.noreply.github.com>
2023-08-22 11:29:00 +0530

    Add files via upload

---
+++ CartApp3/src/main/java/com/bg/service/impl/AbsOffer.java
@@ -0,0 +1,40 @@
+package com.bg.service.impl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.bg.connection.DbCon;
+import com.bg.dao.DiscountItemDao;
+import com.bg.model.CartItem;
+import com.bg.model.Item;
+import com.bg.service.Offer;
+
+public abstract class AbsOffer implements Offer {
+     
+	public ArrayList<String> displayOffer(int pItemId) {
+		ArrayList<String> descList=new ArrayList<String>();
+		try {
+			descList = new DiscountItemDao(DbCon.getConnection()).getOfferDesc(pItemId);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return descList;
+		
+	}
+
+	
+	public int applyOffer(int amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int applyOffer(CartItem c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
